e55371f3e3903bea427abfee14cc4d454c15063c
Sajjad S* <sahhar@outlook.com>
2024-11-02 01:00:15 +0500

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# YouTube Video Link API
+
+This is a simple Flask API that retrieves YouTube video links and provides additional information about the videos.
+
+## Features
+
+- Retrieve the highest resolution video URL from a given YouTube link.
+- Get additional details such as:
+    - Video title
+    - Video description
+    - Video duration (in seconds)
+    - Upload date of the video
+
+## Requirements
+
+- Python 3.x
+- Flask
+- pytubefix
+
+## Installation
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/sajjadsarwarpk/yt-download.git
+   cd yt-download
---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from pytubefix import YouTube
+from pytubefix.exceptions import VideoUnavailable  # Import specific exceptions if needed
+
+app = Flask(__name__)
+
+
+# Get YouTube video link
+@app.route('/get_youtube_video_link', methods=['GET'])
+def get_youtube_video_link():
+    name_filter = request.args.get('video_link')
+
+    if not name_filter:
+        return jsonify({'error': 'video_link parameter is required'}), 400
+
+    try:
+        # Get the URL of the YouTube video
+        yt = YouTube(name_filter)
+
+        # Download the video
+        video = yt.streams.get_highest_resolution()
+        video_title = video.title
+        video_url = video.url
+
+        items = {'video_title': video_title, 'video_url': video_url}
+        return jsonify(items), 200
+
+    except VideoUnavailable:
+        return jsonify({'error': 'The video is unavailable.'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
