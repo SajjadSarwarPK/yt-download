1ae432da35e3f80f0e50f0048fadc2f49876284b
Sajjad S* <sahhar@outlook.com>
2024-11-02 02:22:26 +0500

    verified added.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_youtube_video_link():
     try:
         # run_script()
         # Get the URL of the YouTube video
-        yt = YouTube(name_filter, use_po_token=True, oauth_verifier=True, allow_oauth_cache=True)
+        yt = YouTube(name_filter, use_po_token=True, po_token_verifier=('CgtBLWxOdHFNQlREOCimh5W5BjIKCgJQSxIEGgAgEQ%3D%3D', 'MnQL5bEHXyF1vwvd1h5tz6AxqWqTG7FSe02d3L0N13elCwpd7x5FOsk95Si7Hx2uZ9V0MTAaTLG9UdFwnrgylT-OLBqXemfHfviIhhHI4P-9O57B01CSOwEGgkqmwANJEl9BplTUZKCvLKwIzJE8z-Z3huwPyA=='), allow_oauth_cache=True)
 
         # Download the video
         video = yt.streams.get_highest_resolution()
