7057473c6cb5d37ed4e199841e6db3ea911ef3b5
Sajjad S* <sahhar@outlook.com>
2024-11-02 02:25:11 +0500

    verified added.

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
 from pytubefix import YouTube
-from po_token import run_script
+from typing import Tuple
 
 app = Flask(__name__)
+
+
+def my_po_token_verifier() -> Tuple[str, str]:
+    return (
+        'CgtBLWxOdHFNQlREOCimh5W5BjIKCgJQSxIEGgAgEQ%3D%3D',
+        'MnQL5bEHXyF1vwvd1h5tz6AxqWqTG7FSe02d3L0N13elCwpd7x5FOsk95Si7Hx2uZ9V0MTAaTLG9UdFwnrgylT-OLBqXemfHfviIhhHI4P-9O57B01CSOwEGgkqmwANJEl9BplTUZKCvLKwIzJE8z-Z3huwPyA=='
+    )
 
 # Get YouTube video link
 @app.route('/get_youtube_video_link', methods=['GET'])
@@ -15,7 +22,7 @@ def get_youtube_video_link():
     try:
         # run_script()
         # Get the URL of the YouTube video
-        yt = YouTube(name_filter, use_po_token=True, po_token_verifier=('CgtBLWxOdHFNQlREOCimh5W5BjIKCgJQSxIEGgAgEQ%3D%3D', 'MnQL5bEHXyF1vwvd1h5tz6AxqWqTG7FSe02d3L0N13elCwpd7x5FOsk95Si7Hx2uZ9V0MTAaTLG9UdFwnrgylT-OLBqXemfHfviIhhHI4P-9O57B01CSOwEGgkqmwANJEl9BplTUZKCvLKwIzJE8z-Z3huwPyA=='), allow_oauth_cache=True)
+        yt = YouTube(name_filter, use_po_token=True, po_token_verifier=my_po_token_verifier, allow_oauth_cache=True)
 
         # Download the video
         video = yt.streams.get_highest_resolution()
